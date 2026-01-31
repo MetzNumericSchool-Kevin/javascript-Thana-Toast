bde6c5bc4a8ee3feb6394b8d2e047c057a48cc5f
[Thana-Toast] <erebe57@gmail.com>
2026-01-31 21:38:44 +0100

    11_affichage_infos_potion

--- 1_Basics/exercices/11_affichage_infos_potion.js
+++ 1_Basics/exercices/11_affichage_infos_potion.js
@@ -3,6 +3,10 @@
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potion_soin = {
   nom: "Potion de soin",
-  prix: 50,
-  stock: 10,
+  prix: 42,
+  stock: 25,
 };
+
+console.log(potion_soin.nom);
+
+console.log(potion_soin["prix"]);
