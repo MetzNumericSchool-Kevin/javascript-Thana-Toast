811d27653c62d6d999c26f79fa563c0dba0f0953
[Thana-Toast] <erebe57@gmail.com>
2026-01-31 15:47:55 +0100

    01_definition_variables

--- 1_Basics/exercices/01_definition_variables.js
+++ 1_Basics/exercices/01_definition_variables.js
@@ -1,1 +1,15 @@
 // Définition de variables
+
+const nom_boutique = "L'effet secondaire";
+let stock_potion_soin = 25;
+let prix_potion_soin = 42;
+let boutique_ouverte = true;
+const monnaie = "🪙";
+
+console.log("La boutique d'Archibald s'apelle " + nom_boutique);
+if (boutique_ouverte) {
+    console.log("La boutique est actuellement ouverte");
+} else {
+    console.log("La boutique est actuellement fermée");
+}
+console.log("Il y a actuellement " + stock_potion_soin + " potions de soin à vendre au prix de " + prix_potion_soin + monnaie + " l'unité.")
