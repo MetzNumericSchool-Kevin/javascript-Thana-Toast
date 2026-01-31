a7634868cd87ce9dd540bdc25f57d2bb814e7102
[Thana-Toast] <erebe57@gmail.com>
2026-01-31 22:29:07 +0100

    12_affichage_inventaire

--- 1_Basics/exercices/12_affichage_inventaire.js
+++ 1_Basics/exercices/12_affichage_inventaire.js
@@ -2,3 +2,33 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const inventaire = [];
+
+const potion_soin = {
+  nom: "Potion de soin",
+  prix: 42,
+  stock: 5
+};
+const potion_mana = {
+  nom: "Potion de mana",
+  prix: 20,
+  stock: 3
+};
+const potion_endurance = {
+  nom: "Potion d'endurance",
+  prix: 30,
+  stock: 2
+};
+
+inventaire.push(potion_soin, potion_mana, potion_endurance);
+
+for (let i = 0; i < inventaire.length; i++) {
+  console.log("Nom: " + inventaire[i].nom);
+  console.log("Prix: " + inventaire[i].prix);
+  console.log("Stock: " + inventaire[i].stock);
+};
+
+for (let i = 0; i < inventaire.length; i++) {
+    for (k in inventaire[i]) {
+        console.log(k + ": " + inventaire[i][k]);
+    }
+};
