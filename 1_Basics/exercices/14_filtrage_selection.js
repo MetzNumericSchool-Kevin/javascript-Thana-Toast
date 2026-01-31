31b124725237093d7e26e882b61b635c872d7d15
[Thana-Toast] <erebe57@gmail.com>
2026-01-31 22:58:47 +0100

    14_filtrage_selection

--- 1_Basics/exercices/14_filtrage_selection.js
+++ 1_Basics/exercices/14_filtrage_selection.js
@@ -8,3 +8,24 @@ const inventaire = [
   { nom: "Potion de force", prix: 60, stock: 3 },
   { nom: "Potion de vitesse", prix: 35, stock: 8 }
 ];
+
+const potions_en_stock = [];
+
+for (let i = 0; i < inventaire.length; i++) {
+  if (inventaire[i].stock > 0) {
+    potions_en_stock.push(inventaire[i]);
+  }
+};
+
+const potions_abordables = [];
+
+for (let i = 0; i < inventaire.length; i++) {
+  if (inventaire[i].prix < 40) {
+    potions_abordables.push(inventaire[i]);
+  }
+};
+
+console.log("Il y a " + potions_en_stock.length + " potions en stock");
+console.log(potions_en_stock);
+console.log("Il y a " + potions_abordables.length + " potions abordables");
+console.log(potions_abordables);
