a87e4046aaf009944b985d7e27030a96721190b0
[Thana-Toast] <erebe57@gmail.com>
2026-01-31 15:54:32 +0100

    02_affichage_conditionnel

--- 1_Basics/exercices/02_affichage_conditionnel.js
+++ 1_Basics/exercices/02_affichage_conditionnel.js
@@ -1,5 +1,11 @@
 // Affichage conditionnel
 
 // Variables de l'exercice précédent (modifie selon tes valeurs)
-const nom_boutique = "La Potion Enchantée";
-const boutique_ouverte = true;
+const nom_boutique = "L'effet secondaire";
+let boutique_ouverte = true;
+
+if (boutique_ouverte) {
+    console.log("Bienvenue dans la boutique " + nom_boutique + " Aventurier ! 🎉")
+} else {
+    console.log("La boutique " + nom_boutique + " est fermée, revenez plus tard Aventurier ! 😴")
+}
