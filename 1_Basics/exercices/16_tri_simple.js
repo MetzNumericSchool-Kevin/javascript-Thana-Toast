7d8bf5286b38e0ab5558de27766795c679ade82b
[Thana-Toast] <erebe57@gmail.com>
2026-01-31 23:25:06 +0100

    16_tri_simple

--- 1_Basics/exercices/16_tri_simple.js
+++ 1_Basics/exercices/16_tri_simple.js
@@ -7,3 +7,19 @@ const inventaire = [
   { nom: "Potion de soin", prix: 50, stock: 10 },
   { nom: "Potion d'endurance", prix: 40, stock: 7 }
 ];
+
+for (let i = 0; i < inventaire.length; i++) {
+  for (let j = 0; j < inventaire.length; j++) {
+    if (inventaire[i].prix < inventaire[j].prix) {
+      let temp = inventaire[i];
+      inventaire[i] = inventaire[j];
+      inventaire[j] = temp;
+    }
+  }
+};
+
+console.log(inventaire);
+
+for (let i = 0; i < inventaire.length; i++) {
+  console.log(inventaire[i].nom + " : " + inventaire[i].prix + "🪙.");
+};
