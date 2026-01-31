0de8649d9d21a1b6b26e10db19a28b229c9c0373
[Thana-Toast] <erebe57@gmail.com>
2026-01-31 21:33:53 +0100

    08_ajout_nouvelle_potion

--- 1_Basics/exercices/08_ajout_nouvelle_potion.js
+++ 1_Basics/exercices/08_ajout_nouvelle_potion.js
@@ -2,3 +2,8 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+potions.push("Huile de coude");
+console.log(potions);
+potions.pop();
+console.log(potions);
