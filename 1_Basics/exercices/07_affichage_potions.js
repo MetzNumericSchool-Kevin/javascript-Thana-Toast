9d7551d8e16aefbfc553f79c4f4106a6b41deb2a
[Thana-Toast] <erebe57@gmail.com>
2026-01-31 21:29:28 +0100

    07_affichage_potions

--- 1_Basics/exercices/07_affichage_potions.js
+++ 1_Basics/exercices/07_affichage_potions.js
@@ -2,3 +2,11 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+console.log(potions[0]);
+
+console.log(potions[potions.length - 1]);
+
+for (let i =0; i < potions.length; i++) {
+    console.log("Nous avons de la " + potions[i]);
+}
