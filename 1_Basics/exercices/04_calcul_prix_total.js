a57b11e76506d739dbf692037728e58c8b36a404
[Thana-Toast] <erebe57@gmail.com>
2026-01-31 16:23:27 +0100

    04_calcul_prix_total

--- 1_Basics/exercices/04_calcul_prix_total.js
+++ 1_Basics/exercices/04_calcul_prix_total.js
@@ -1,4 +1,11 @@
 // Calcul du prix total d'une commande de potion 🪙
 
 // Variables des exercices précédents (modifie selon tes valeurs)
-let prix_potion_soin = 50;
+let stock_potion_soin = 25;
+let prix_potion_soin = 42;
+const monnaie = "🪙";
+
+quantite_voulue = prompt("Combien de potions de soin veux-tu acheter ? ");
+const quantite_voulue_nombre = parseInt(quantite_voulue);
+
+console.log("Prix de " + quantite_voulue_nombre + " potions de soins : " + quantite_voulue_nombre * prix_potion_soin + monnaie + " mon cher Aventurier. 💸")
